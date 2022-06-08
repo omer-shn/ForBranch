ff63068c3c29ae9face507f5fba364010926b3b1
= <omershnopium@gmail.com>
2022-06-09 00:37:05 +0300

    info second

--- src/HiltiBranch.java
+++ src/HiltiBranch.java
@@ -1,6 +1,7 @@
 public class HiltiBranch {
     public static void main(String[] args) {
 
-        System.out.println("9 haziran");
+        System.out.println("9 june");
+        System.out.println("yesterday was june 8");
     }
 }
