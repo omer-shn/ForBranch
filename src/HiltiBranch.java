dabd5b39f005750f6317d94790ad1e3c876ac96e
= <omershnopium@gmail.com>
2022-06-09 00:44:03 +0300

    info third

--- src/HiltiBranch.java
+++ src/HiltiBranch.java
@@ -3,5 +3,6 @@ public class HiltiBranch {
 
         System.out.println("9 june");
         System.out.println("yesterday was june 8");
+        System.out.println("previus day was june 7");
     }
 }
